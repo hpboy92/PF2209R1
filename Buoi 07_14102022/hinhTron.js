5394ba379b216fa0b9e0e6b37d2a9506d47aa835
Phạm Hoàng Anh 01 <Anh1.PhamHoang@mbs.com.vn>
2022-10-18 13:17:29 +0700

    bai tap hinh tron

---
+++ Buoi 07_14102022/hinhTron.js
@@ -0,0 +1,9 @@
+let r = prompt("Nhập Bán Kính: ");
+const PI = 3.14;
+let D,C,S;
+D = 2*r;
+C = 2*PI*r;
+S = PI*r*r;
+document.write(`Đường Kính Là ${D}`);
+document.write(`Chu Vi Là ${C}`);
+document.write(`Diện Tích Là ${S}`);
